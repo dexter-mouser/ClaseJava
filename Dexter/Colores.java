af7c7e3a958a1d7736b81d377d56ebfb2045d703
jose-b <josembotero17@gmail.com>
2026-08-25 12:10:35 -0500

    Add files via upload

---
+++ Dexter/Colores.java
@@ -0,0 +1,11 @@
+public class Colores {
+    public static void main(String[] args) {
+        String color;
+        color.equals("azul");
+        color.equals("verde");
+        color.equals("rojo");
+        if (color.equals("azul"));
+        System.out.println("azul");
+        
+    }
+}
---
+++ Dexter/Condicional.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Condicional {
+public static void main(String[] args) {
+        Scanner aedadPaquito = new Scanner(System.in);
+        int edadPaquito;
+}
+}   
