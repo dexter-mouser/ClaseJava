e62d1cc61e788b7531d6322487b056b990820bb0
Jbotero <josembotero17@gmail.com>
2026-09-21 23:15:19 -0500

    feat/login-momento1

---
+++ JS/Pag-Button/login.js
@@ -0,0 +1,8 @@
+function validarAcceso() {
+    const USUARIO_CORRECTO = "admin";
+    const CONTRASENA_CORRECTA = "1234";
+    //*
+    const MAX_INTENTOS = 3;
+    let intentos = 0;
+    let acceso = false;
+}
